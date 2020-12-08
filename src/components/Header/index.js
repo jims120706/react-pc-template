46543c2a1ccad040e7f88664c800cfd639d03c02
liangjiajun <liangjiajun@yaeher.com>
2020-12-08 09:56:32 +0800

    feat：退出登录添加

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,13 +1,27 @@
 import React from 'react'
 import style from './index.module.scss'
+import { connect } from 'react-redux'
 
-export default class Header extends React.Component {
+class Header extends React.Component {
   render() {
+    const { logout } = this.props
     return (
       <div className={style['header']}>
         <div className={style['logo']}>logo</div>
         <div className={style['title']}>标题</div>
+        <div onClick={logout}>退出登录</div>
       </div>
     )
   }
 }
+
+const mapDispatchToProps = {
+  logout: () => {
+    return { type: 'CLEAR_TOKEN', module: 'user' }
+  },
+}
+
+export default connect(
+  null,
+  mapDispatchToProps,
+)(Header)
